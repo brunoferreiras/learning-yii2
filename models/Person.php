9931fe1a204ef7f583ac8982d1583ee539ae0318
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-12-30 11:30:54 -0300

    How to use gii

--- models/Person.php
+++ models/Person.php
@@ -2,12 +2,49 @@
 
 namespace app\models;
 
-use yii\db\ActiveRecord;
+use Yii;
 
-class Person extends ActiveRecord
+/**
+ * This is the model class for table "people".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $city
+ * @property string $state
+ * @property string $email
+ */
+class Person extends \yii\db\ActiveRecord
 {
+    /**
+     * @inheritdoc
+     */
     public static function tableName()
     {
         return 'people';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'city', 'state', 'email'], 'required'],
+            [['name', 'city', 'state', 'email'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'city' => 'City',
+            'state' => 'State',
+            'email' => 'Email',
+        ];
     }
 }
